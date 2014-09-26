4960354a46792b6468318981166ecd3b024e3e58
wmira <warrenmira@gmail.com>
2014-09-26 22:29:39 +0800

    fixed loop

--- src/euler/euler7.scala
+++ src/euler/euler7.scala
@@ -1,11 +1,18 @@
 package euler
 
+/**
+ * 
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+   What is the 10 001st prime number?
+ * 
+ */
 object euler7 {
 	
   def main(args : Array[String]) = {
     var primes = 0
     var tocheck = 1
-    while ( true ) {
+    while ( primes < 10001 ) {
       if ( eulerutils.isPrime(tocheck) ) {
         primes += 1
         if ( primes == 10001 ) {
@@ -15,6 +22,7 @@ object euler7 {
       tocheck += 1
     }      
   }
+  
   
   
 }

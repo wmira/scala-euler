ef8489ad519e520f76d5680b48422efe0a862a70
Warren Mira <warrenmira@gmail.com>
2014-09-27 13:25:22 +0800

    Update eulerutils.scala

--- src/euler/eulerutils.scala
+++ src/euler/eulerutils.scala
@@ -4,20 +4,20 @@ object eulerutils {
   
 	def isPrime(input: BigDecimal) : Boolean = {
     
-    var primeCheck = 2
+	    var primeCheck = 2
+	    
+	    //FIXME: is there a better way to do this shit?
+	    while ( true ) {    	
+	      if ( input % primeCheck == 0 ) {
+	        return false
+	      }
+	      if ( primeCheck * primeCheck > input ) {
+	        return true
+	      }
+	      primeCheck += 1
+	      
+	    }
+	    return false
     
-    //FIXME: is there a better way to do this shit?
-    while ( true ) {    	
-      if ( input % primeCheck == 0 ) {
-        return false
-      }
-      if ( primeCheck * primeCheck > input ) {
-        return true
-      }
-      primeCheck += 1
-      
-    }
-    return false
-    
-  }
+  	}
 }

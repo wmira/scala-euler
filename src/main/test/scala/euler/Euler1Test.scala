ba39c494650df34b7a6035196679cec54230b4e6
warren <warren@miralabs.net>
2014-09-30 06:52:39 +0800

    adding test and refactoring stuff

---
+++ src/main/test/scala/euler/Euler1Test.scala
@@ -0,0 +1,13 @@
+package euler
+
+import org.scalatest.FlatSpec
+
+/**
+ *
+ */
+class Euler1Test extends FlatSpec {
+
+  "Euler1 answer" should " be equal to 233168 " in {
+    Euler1 euler1 = new Euler1()
+  }
+}

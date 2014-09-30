95f8a41507c8b071f99a97311be4060f9fdd9085
warren <warren@miralabs.net>
2014-09-30 08:16:00 +0800

    complete this

---
+++ src/main/scala/euler/Euler3.scala
@@ -0,0 +1,14 @@
+package euler
+
+/**
+ *  The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ *   What is the largest prime factor of the number 600851475143 ?
+ */
+class Euler3 extends EulerSolver {
+
+
+  override def solve(): Long = {
+    -1L
+  }
+}
---
+++ src/test/scala/euler/Euler3Test.scala
@@ -0,0 +1,17 @@
+package euler
+
+import org.junit.Test
+
+/**
+ * Created by miralabs on 9/30/2014.
+ */
+class Euler3Test {
+
+  @Test def euler3() = {
+
+    val euler = new Euler3
+    assertEquals(0,euler.solve)
+
+  }
+
+}
